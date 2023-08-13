4ee411ae89de50af339985a12e538ea4621e4e5c
Cyperfroz00 <pen_ponlok@pp.bbu.edu.kh>
2023-08-13 13:04:38 +0700

    Finished Project

---
+++ android/app/src/main/kotlin/com/ponlok/aba_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ponlok.aba_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        drawer: Drawer(
+        ),
+        appBar: AppBar(
+          title: Text('ABA Mobile'),
+          actions: [
+            Icon(Icons.notifications),
+            Icon(Icons.call),
+            Container(
+              child: Container(
+              ),
+            )
+          ],
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Flexible(
+                flex: 3,
+                child: Container(
+                  child: GridView.count(crossAxisCount: 3,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(left:5.0,top: 5),
+                        color: Colors.indigo,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.menu),
+                            Text('Account'),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Container(
+                  color: Colors.indigo,
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Container(
+                  color: Colors.red,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
